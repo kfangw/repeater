8ae746a6cfb160d1c487ad0660d965e27adcd31b
Swen Mun <longfinfunnel@gmail.com>
2021-06-17 00:59:19 +0900

    Daemonize

--- Program.cs
+++ Program.cs
@@ -23,12 +23,14 @@ namespace repeater
             CoconaLiteApp.Run<Program>(args);
         }
 
-        [Command(Description = "Send empty transaction as ping")]
-        public async Task Send(
+        [Command(Description = "Send empty transaction as ping repeatly.")]
+        public async Task Run(
             [Argument]
             string endpoint,
             [Argument]
-            string privateKey
+            string privateKey,
+            [Argument]
+            int delayMilleSeconds
         )
         {
             using var gqlClient = new GraphQLHttpClient(
@@ -36,29 +38,35 @@ namespace repeater
                 new NewtonsoftJsonSerializer()
             );
             var pk = new PrivateKey(ByteUtil.ParseHex(privateKey));
-            var nonceRequest = new GraphQLRequest
-            {
-                Query = $"{{ nextTxNonce(address:\"{ pk.ToAddress().ToHex()}\") }}",
-            };
-            var nonce = (await gqlClient.SendQueryAsync<dynamic>(nonceRequest)).Data.nextTxNonce;
-            Console.WriteLine(nonce);
 
-            var tx = Transaction<PolymorphicAction<ActionBase>>.Create(
-                (long)nonce, 
-                pk, 
-                genesisHash, 
-                new PolymorphicAction<ActionBase>[] { },
-                null,
-                DateTimeOffset.UtcNow
-            );
-            var txHex = ByteUtil.Hex(tx.Serialize(true));
-
-            var txRequest = new GraphQLRequest
+            while (true)
             {
-                Query = $"mutation {{ stageTx(payload: \"{txHex}\") }}",
-            };
-            var res = await gqlClient.SendMutationAsync<dynamic>(txRequest);
-            Console.WriteLine(res.Data.stageTx);
+                var nonceRequest = new GraphQLRequest
+                {
+                    Query = $"{{ nextTxNonce(address:\"{ pk.ToAddress().ToHex()}\") }}",
+                };
+                var nonce = (await gqlClient.SendQueryAsync<dynamic>(nonceRequest)).Data.nextTxNonce;
+                Console.WriteLine(nonce);
+
+                var tx = Transaction<PolymorphicAction<ActionBase>>.Create(
+                    (long)nonce,
+                    pk,
+                    genesisHash,
+                    new PolymorphicAction<ActionBase>[] { },
+                    null,
+                    DateTimeOffset.UtcNow
+                );
+                var txHex = ByteUtil.Hex(tx.Serialize(true));
+
+                var txRequest = new GraphQLRequest
+                {
+                    Query = $"mutation {{ stageTx(payload: \"{txHex}\") }}",
+                };
+                var res = await gqlClient.SendMutationAsync<dynamic>(txRequest);
+                Console.WriteLine(res.Data.stageTx);
+                
+                await Task.Delay(delayMilleSeconds);
+            }
         }
     }
 }
